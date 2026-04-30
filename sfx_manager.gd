9165cc8503f88f7fcb76ea8e1d5ddacc287d40ce
Nina <calmon.nina01@gmail.com>
2026-04-30 12:38:07 -0300

    Simple changes were made, and a bug in sfx_manager was fixed.

--- sfx_manager.gd
+++ sfx_manager.gd
@@ -3,11 +3,13 @@ extends Node
 func play_sound(audio_player: AudioStreamPlayer):
 	if !get_tree().current_scene:
 		return                                                                                                                                                                                                                                  
-	var new_audio_player = audio_player.duplicate()
+	var new_audio_player: AudioStreamPlayer = audio_player.duplicate()
 	get_tree().current_scene.add_child(new_audio_player)
 	
 	new_audio_player.play()
-																	  
-	new_audio_player.finished.connect(func():
-		new_audio_player.queue_free()
-	)
+	
+	new_audio_player.finished.connect(queue_free_audio_player.bind(new_audio_player))
+
+func queue_free_audio_player(audio_player: AudioStreamPlayer):
+	if audio_player:
+		audio_player.queue_free()
