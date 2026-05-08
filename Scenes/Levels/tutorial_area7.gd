9898c7cfa549b12bb8bcd9dcda97dd249d71ebbe
Nina <calmon.nina01@gmail.com>
2026-05-08 20:21:41 -0300

    Final ver!!!!!

--- Scenes/Levels/tutorial_area7.gd
+++ Scenes/Levels/tutorial_area7.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var rigid_body_2d: RigidBody2D = $RigidBody2D
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -8,3 +9,4 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player and rigid_body_2d:
 		rigid_body_2d.queue_free()
+		audio_stream_player.play()
