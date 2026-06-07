80553c66e3a30c279d9688a2e637c147eec062e6
Nina <calmon.nina01@gmail.com>
2026-06-07 04:00:19 -0300

    First version of final level design finished.

--- Scenes/Spaceship/door.gd
+++ Scenes/Spaceship/door.gd
@@ -26,7 +26,7 @@ func _on_clicked():
 	SFXManager.play_sound(door_sfx)
 	await get_tree().create_timer(1.2).timeout
 	StatsManager.day += 1
-	LevelTransition.change_scene_to("res://Scenes/Levels/Level1.tscn", 3, 2)
+	LevelTransition.change_scene_to("res://Scenes/Levels/LevelFinal2.tscn", 3, 2)
 
 func _on_focus_changed(focus: bool, _subject: Node2D):
 	## If there is a race condition with the activation of the clickable_highlight here and the disabling of it
