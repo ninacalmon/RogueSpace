83197ccb8e27e6819cac5923af1bdc4992162dff
Nina <calmon.nina01@gmail.com>
2026-06-14 18:32:35 -0300

    Implement sound settings container on menu and pause overlay and adjust audio channel for some audio stream players.

--- Scenes/Levels/tutorial_area7.gd
+++ Scenes/Levels/tutorial_area7.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @onready var rigid_body_2d: RigidBody2D = $RigidBody2D
-@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var audio_stream_player: AudioStreamPlayer = $CritterSound
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
